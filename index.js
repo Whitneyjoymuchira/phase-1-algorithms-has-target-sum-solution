4d77ebeb1489274ac2f76112ac5f9604698c60d0
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-17 23:29:02 +0300

    solution

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
-
+//0(n)
+//space complexity
+//0(n)
 /* 
   Add your pseudocode here
 */
+
 
 /*
   Add written explanation of your solution here
@@ -18,17 +32,17 @@ function hasTargetSum(array, target) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("=>", hasTargetSum([2, 8, 23, 4, 14, 7], 11));
 
   console.log("");
 
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("=>", hasTargetSum([22, 11, 12, 6, 30], 42));
 
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  console.log("=>", hasTargetSum([4, 8, 5], 11));
 }
 
 module.exports = hasTargetSum;
